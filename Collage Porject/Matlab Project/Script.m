04eaec110609dd24c5f21ff85098a51b2074f2f5
Salah Hassan <142039033+SalahHassan202@users.noreply.github.com>
2025-03-10 19:10:09 +0200

    My university projects

---
+++ Collage Porject/Matlab Project/Script.m
@@ -0,0 +1,8 @@
+%%%% Team 8 
+Va = 100 ; % Armature Input Voltage
+Ra = 0.4 ; % Armature Resistance
+La = 0.4 ; % Armature Inductance
+B = 0.02 ; % Damping Coefficient (Due Friction)
+J = 7    ; % Motor Body Inertia (J = m.r^2)
+Tl = 30  ; % Load Torque 
+K = 1.4  ; % constant
